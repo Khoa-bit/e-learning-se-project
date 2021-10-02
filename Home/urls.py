113c83422a085ba63482196a4b598e8d08f7dc81
DBDuy <dbduy2001@gmail.com>
2021-10-02 22:51:46 +0700

    front end stuff

--- Home/urls.py
+++ Home/urls.py
@@ -4,4 +4,5 @@ from django.urls import path,include
 urlpatterns = [
     path('',views.HomeView,name="home"),
     path('user/',include("User.urls")),
+    path('about/',views.AboutView,name="about-page")
 ]
--- Home/views.py
+++ Home/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def HomeView(request):
   return render(request, "Home/home.html")
+
+def AboutView(request):
+  context={"pageinfo":["DBDUY","SE GROUP","2021"]}
+  return render(request, "Home/about.html",context)
