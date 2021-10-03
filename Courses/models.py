2a721da68314e5c3791ee7c9bf497799b0f82698
heroinism <thu.ha2897@gmail.com>
2021-10-04 05:21:44 +0700

    Added Courses app and models.

---
+++ Courses/models.py
@@ -0,0 +1,50 @@
+import datetime
+
+from django.db import models
+from django.db.models import CASCADE, SET_NULL
+from User.models import Lecturer
+
+# Create your models here.
+class Major(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Course(models.Model):
+    major = models.ForeignKey(Major, on_delete=CASCADE)
+    name = models.CharField(max_length=80)
+
+
+class Timetable(models.Model):
+    DAYS_OF_WEEK = [
+        ('MON', 'Monday'),
+        ('TUE', 'Tuesday'),
+        ('WED', 'Wednesday'),
+        ('THU', 'Thursday'),
+        ('FRI', 'Friday'),
+        ('SAT', 'Saturday'),
+        ('SUN', 'Sunday'),
+    ]
+    day_of_the_week = models.CharField(max_length=3, choices=DAYS_OF_WEEK)
+    start_time = models.TimeField(auto_now=False, auto_add_now=False)
+    duration = models.DurationField()
+
+    @property
+    def day_part(self):
+        if (self.start_time <= datetime.time(12)):
+            return 'Morning'
+        else:
+            return 'Afternoon'
+
+    def __str__(self):
+        return self.day_of_the_week + " " + self.day_part
+
+
+class Class(models.Model):
+    course = models.ForeignKey(Course, on_delete=CASCADE)
+    lecturer = models.ForeignKey(Lecturer, on_delete=CASCADE)
+    schedule = models.ForeignKey(Timetable, on_delete=SET_NULL)
+    name = models.CharField(max_length=90)
+
+    def __str__(self):
+        return self.course.name + " - " + self.schedule + " - " + self.lecturer.full_name
+
