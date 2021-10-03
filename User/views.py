b66266479a720ad423a6f121a4608ec5fcba03e4
DB-Duy <toxicphoenix01@gmail.com>
2021-10-03 17:44:46 +0700

    customized admin page for users, redirect after login depending on usertype

--- User/views.py
+++ User/views.py
@@ -1,4 +1,4 @@
-from django.http.response import HttpResponseRedirect
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth import authenticate,login,logout
 from .models import *
@@ -15,7 +15,11 @@ def LoginView(request):
       login(request,user)
       if user.is_staff:
         return HttpResponseRedirect(reverse("admin:index"))
-      return HttpResponseRedirect(reverse("userinfo",args=[user.id]))
+      elif user.is_lecturer():
+        return HttpResponseRedirect(reverse("lecturerinfo",args=[user.lecturer.id]))
+      elif user.is_student():
+        return HttpResponseRedirect(reverse("studentinfo",args=[user.student.id]))
+      else: return HttpResponseRedirect(reverse("userinfo",args=[user.id]))
   else:
     form = AuthenticationForm()
   return render(request,"User/login.html",{"form":form})
@@ -24,11 +28,16 @@ def UserInfoView(request,id):
   user = User.objects.get(id=id)
   if not (request.user.is_authenticated and request.user==user):
     return HttpResponseRedirect(reverse("home"))
-  context={"user":user}
-  if hasattr(user,"student"):
-    context["role"]="student"
-  elif hasattr(user,"lecturer"): context["role"]="Lecturer"
-  else: context["role"]="user"
-  print(context)
-  return render(request,"User/userinfo.html",context)
+  return render(request,"User/userinfo.html")
 
+def LecturerInfoView(request,id):
+  user = Lecturer.objects.get(id=id)
+  if not (request.user.is_authenticated and request.user==user.user_id):
+    return HttpResponseRedirect(reverse("home"))
+  return render(request, "User/Lecturerinfo.html")
+  
+def StudentInfoView(request,id):
+  user = Student.objects.get(id=id)
+  if not (request.user.is_authenticated and request.user==user.user_id):
+    return HttpResponseRedirect(reverse("home"))
+  return render(request, "User/Studentinfo.html")
