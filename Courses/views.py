efaea1985304c34b6694dba5cae1508f0f8dec7d
Anh Khoa <55542012+Khoa-bit@users.noreply.github.com>
2021-10-07 21:40:50 +0700

    ✏️ Port student-classes URL to active-courses

--- Courses/views.py
+++ Courses/views.py
@@ -20,13 +20,14 @@ def StudentSchedule(request, id):
     context = {'registered_classes': registered_classes, 'title': 'Student Schedule'}
     return render(request, 'Courses/schedule.html', context)
 
-def ClassesPage(request, id):
-    user = Student.objects.get(id=id)
-    registered_classes = []
-    for i in user.class_id.all():
-        registered_classes.append(i)
-    context = {'registered_classes': registered_classes}
-    return render(request, 'Courses/registered_classes.html', context)
+# Port to ActiveCourses
+# def ClassesPage(request, id):
+#     user = Student.objects.get(id=id)
+#     registered_classes = []
+#     for i in user.class_id.all():
+#         registered_classes.append(i)
+#     context = {'registered_classes': registered_classes}
+#     return render(request, 'Courses/registered_classes.html', context)
 
 #def SpecificClass(request, student_id, class_id):
 #    return HttpResponse('<h1>ay lmao waddup homie</h1>')
