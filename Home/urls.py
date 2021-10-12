b13539dca87dfe1c4eb8529cb18d30dedb683dcb
RexeonNova <hoclamhuynh@gmail.com>
2021-10-12 15:51:48 +0700

    add announcement page. html format???

--- Home/urls.py
+++ Home/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('user/', include("User.urls")),
     path('about/', views.AboutView, name="about-page"),
     path('index/', views.IndexView, name="index-page"),
+    path('announcement/<int:announcement_id>', views.GuestAnnouncementPage, name='guest-announcement-viewpage')
 ]
-
