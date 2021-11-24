8fecf0eacf5ed4d884142dcf6733ec21fc7aaf23
Anh Khoa <55542012+Khoa-bit@users.noreply.github.com>
2021-11-24 18:34:38 +0700

    refactor: 🔨add "time_modified" field to Home_announcement Table

--- Home/models.py
+++ Home/models.py
@@ -8,6 +8,7 @@ class Announcement(models.Model):
   title = models.CharField(max_length=100)
   content = models.TextField(max_length=1000)
   time_created = models.DateTimeField(auto_now_add=True)
+  time_modified = models.DateTimeField(auto_now=True)
 
   def __str__(self):
     return self.title
