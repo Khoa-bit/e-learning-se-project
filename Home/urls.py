9ac735afe67710e24fa388d4ed99788b27a32971
Anh Khoa <55542012+Khoa-bit@users.noreply.github.com>
2021-12-01 23:45:49 +0700

    fix: 🐛fix view all and search general announcements

--- Home/urls.py
+++ Home/urls.py
@@ -4,7 +4,7 @@ from django.urls import path,include
 urlpatterns = [
     path('', views.GuestAnnouncement, name="guest-announcement-page"),
     path('user/', include("User.urls")),
-    path('announcement/<int:id>', views.GuestAnnouncementPage, name='guest-announcement-viewpage'),
+    path('announcement/<int:announcement_id>', views.GuestAnnouncementPage, name='guest-announcement-viewpage'),
     path('about/', views.About, name='about'),
     path('contact-us/', views.Contact, name='contact'),
     path('announcement/all/', views.GuestAnnouncementAll, name='guest-announcement-all'),
